4db28e029eca626ec59ed2be2380bc12c2da8a2f
mae masters <maemastersdev@gmail.com>
2022-07-31 14:06:16 +0100

    baby names are now colour coded and listed alphabetically

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
 import fullBabyData from "./baby-names-data.json"
 import { babyData } from "./utils/name-interface";
+import {DisplayAllNames} from "/home/2206-015-mm/Developer/academy/training/react-apps/baby-names-app/src/components/mainView";
 import '/home/2206-015-mm/Developer/academy/training/react-apps/baby-names-app/src/App.css';
 import {sortNamesAlphabetically} from "/home/2206-015-mm/Developer/academy/training/react-apps/baby-names-app/src/utils/sort-alphabetically";
 
 //import { showAllNames } from "./components/showAllNames";
 
 function App(): JSX.Element {
-  
-  function makeBabyNameElement(oneBaby: babyData) {
     return (
-    <>
-      {oneBaby.sex === 'm' && (
-    <button className = "boyName"> {oneBaby.name}</button>
-    )}
-      {oneBaby.sex === 'f' && (
-        <button className = "girlName"> {oneBaby.name} </button>
-      )}
-    </>
+      <DisplayAllNames />
     );
-      }
-  return (
-    <>
-    <body className= "body">
-    <p> There are {fullBabyData.length} baby names!</p>
-    <div className= "mainList">
-      {fullBabyData.map(makeBabyNameElement)}
-    </div>
-    </body>
-    </>
-  );
-  }
+}
 
 export default App;
--- src/components/mainView.tsx
+++ src/components/mainView.tsx
@@ -1,4 +1,48 @@
-import { showAllNames } from "./showAllNames";
 import { babyData } from "../utils/name-interface";
 import fullBabyData from "../baby-names-data.json";
+import { sortNamesAlphabetically } from "../utils/sort-alphabetically";
 
+export function DisplayAllNames(): JSX.Element {
+
+    function sortAlphabetically(a: babyData,b: babyData) {
+            if (a.name < b.name){
+                return -1
+            }
+            else if (a.name > b.name) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+
+    const sortedBabyNameData: babyData[] = [...fullBabyData].sort((a,b) => sortAlphabetically(a, b));
+          
+    
+    function makeBabyNameElement(oneBaby: babyData) {
+      return (
+      <>
+        {oneBaby.sex === 'm' && (
+      <button className = "boyName"> {oneBaby.name}</button>
+      )}
+        {oneBaby.sex === 'f' && (
+          <button className = "girlName"> {oneBaby.name} </button>
+        )}
+      </>
+      );
+        }
+   
+    const unsortedBabyData = sortedBabyNameData.map(makeBabyNameElement);
+
+    return (
+      <>
+      <body className= "body">
+      <h1> Baby Name App!</h1>
+      <p> There are {fullBabyData.length} baby names!</p>
+      <div className= "mainList">
+        {unsortedBabyData}
+      </div>
+      </body>
+      </>
+    );
+    }
